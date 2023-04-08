d3e2302f4a11dcee4e789b72179e2768cb3503df
Simon <44496148+siim0n@users.noreply.github.com>
2023-04-08 17:35:28 +0200

    Oui

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,4 +2,7 @@ import Server from "./Server.js";
 import Database from "./Database.js";
 const server = new Server();
 server.start(3001);
-console.log(server.db.getMinersPoints());
+const points = server.db.getMinersPoints();
+for (const point of points) {
+    console.log(point);
+}
